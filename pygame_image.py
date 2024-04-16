be7f15d88e66841f9f3a4c3fdc968ae0a4be606c
dinith <c0b2302099@edu.teu.ac.jp>
2024-04-16 17:53:18 +0900

    演習課題1

--- pygame_image.py
+++ pygame_image.py
@@ -24,10 +24,10 @@ def main():
             kk_rct.move_ip([0, -1])
         if key_lst[pg.K_DOWN]:
             kk_rct.move_ip([0, +1])
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip([-1, 0])
+        #if key_lst[pg.K_LEFT]:
+        kk_rct.move_ip([-1, 0])
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip([+1, 0])
+            kk_rct.move_ip([2, 0])
 
 
 
